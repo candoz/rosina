a694b02359ec9aabec88b95eaf3a5dd82d801561
Marco Canducci <marco.candoz@gmail.com>
2019-10-17 22:58:00 +0200

    Add finite state constants

--- controller.lua
+++ controller.lua
@@ -3,27 +3,15 @@ motor_schemas = require "motor_schemas"
 motor_conversions = require "motor_conversions"
 
 local MAX_SPEED = 10
+local SEARCH, ON_NEST, EXPLORE_CHAIN, CHAIN_LINK, ON_PREY = 1, 2, 3, 4, 5
+local current_state = SEARCH
 
 function init()
   robot.leds.set_all_colors("black")
 end
 
 function step()
-  if robot.motor_ground[1].value > 0.9 or
-      robot.motor_ground[2].value > 0.9 or
-      robot.motor_ground[3].value > 0.9 or
-      robot.motor_ground[4].value > 0.9 then
-    robot.leds.set_all_colors("green")
-
-  elseif robot.motor_ground[1].value < 0.1 or
-      robot.motor_ground[2].value < 0.1 or
-      robot.motor_ground[3].value < 0.1 or
-      robot.motor_ground[4].value < 0.1 then
-    robot.leds.set_all_colors("red")
-
-  else
-    robot.leds.set_all_colors("black")
-  end
+  set_leds_color()
 
   local straight = motor_schemas.move_straight()
   local random = motor_schemas.move_random()
@@ -43,4 +31,19 @@ function step()
   local vel_l, vel_r = motor_conversions.vec_to_vels(sum, robot.wheels.axis_length)
   -- log("vel_l: " .. vel_l .. " vel_r: " .. vel_r)
   robot.wheels.set_velocity(vel_l, vel_r)
+end
+
+function set_leds_color()
+  if robot.motor_ground[1].value > 0.9 or robot.motor_ground[2].value > 0.9 or
+      robot.motor_ground[3].value > 0.9 or robot.motor_ground[4].value > 0.9 then
+    robot.leds.set_all_colors("green")
+
+  elseif robot.motor_ground[1].value < 0.1 or robot.motor_ground[2].value < 0.1 or
+      robot.motor_ground[3].value < 0.1 or robot.motor_ground[4].value < 0.1 then
+    robot.leds.set_all_colors("red")
+
+  else
+    robot.leds.set_all_colors("black")
+
+  end
 end
