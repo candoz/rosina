b9849a0335a6a3773f41f9160c2736a4e3d9ab72
Daniele Schiavi <schiavi91@gmail.com>
2019-10-18 12:14:30 +0200

    Add search state implemenatation

--- controller.lua
+++ controller.lua
@@ -5,6 +5,7 @@ motor_conversions = require "motor_conversions"
 local MAX_SPEED = 10
 local SEARCH, ON_NEST, EXPLORE_CHAIN, CHAIN_LINK, ON_PREY = 1, 2, 3, 4, 5
 local current_state = SEARCH
+local motor_vector = {length = 0, angle = 0}
 
 function init()
   robot.leds.set_all_colors("black")
@@ -13,22 +14,9 @@ end
 function step()
   robot.leds.set_all_colors(check_ground())
 
-  local straight = motor_schemas.move_straight()
-  local random = motor_schemas.move_random()
-  local avoid_mono = motor_schemas.avoid_collisions_monosensor()
-  local avoid_multi = motor_schemas.avoid_collisions_multisensor()
-  local move_perpendicular = motor_schemas.move_perpendicular_monosensor()
-  local sum = vector.vec2_polar_sum(straight, random)
-  sum = vector.vec2_polar_sum(sum, avoid_mono)
-  -- sum = vector.vec2_polar_sum(sum, avoid_multi)
-  sum = vector.vec2_polar_sum(sum, move_perpendicular)
-  -- log("straight.length: " .. straight.length .. " straight.angle: " .. straight.angle)
-  -- log("random.length: " .. random.length .. " random.angle: " .. random.angle)
-  log("mono.length: " .. avoid_mono.length .. " mono.angle: " .. avoid_mono.angle)
-  log("multi.length: " .. avoid_multi.length .. " multi.angle: " .. avoid_multi.angle)
-  log("perpendicular.length: " .. move_perpendicular.length .. " perpendicular.angle: " .. move_perpendicular.angle)
-  -- log("sum.length: " .. sum.length .. " sum.angle: " .. sum.angle)
-  local vel_l, vel_r = motor_conversions.vec_to_vels(sum, robot.wheels.axis_length)
+  motor_vector = search()
+  
+  local vel_l, vel_r = motor_conversions.vec_to_vels(motor_vector, robot.wheels.axis_length)
   -- log("vel_l: " .. vel_l .. " vel_r: " .. vel_r)
   robot.wheels.set_velocity(vel_l, vel_r)
 end
@@ -49,7 +37,22 @@ function check_ground()
 end
 
 function search()
-  return {length = 0, angle = 0}
+  local resulting_vector = {length = 0, angle = 0}
+  log(check_ground())
+  if check_ground() == "green" then
+    current_state = ON_NEST
+  else
+    local straight = motor_schemas.move_straight()
+    local random = motor_schemas.move_random()
+    local avoid_mono = motor_schemas.avoid_collisions_monosensor()
+    local avoid_multi = motor_schemas.avoid_collisions_multisensor()
+    local move_perpendicular = motor_schemas.move_perpendicular_monosensor()
+    resulting_vector = vector.vec2_polar_sum(straight, random)
+    resulting_vector = vector.vec2_polar_sum(resulting_vector, avoid_mono)
+    resulting_vector = vector.vec2_polar_sum(resulting_vector, avoid_multi)
+    resulting_vector = vector.vec2_polar_sum(resulting_vector, move_perpendicular)
+  end
+  return resulting_vector
 end
 
 function on_nest()
